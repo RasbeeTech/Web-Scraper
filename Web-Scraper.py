f5db0b29fdda3f010034bc19dfac2807696fab21
MykalB081 <78550307+MykalB081@users.noreply.github.com>
2021-02-13 17:46:58 -0500

    Update Web-Scraper.py
    - iterates through list of urls

--- Web-Scraper.py
+++ Web-Scraper.py
@@ -4,18 +4,15 @@ from bs4 import BeautifulSoup
 import time
 import csv
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 
-def get_driver():
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-
-    driver = webdriver.Chrome('./chromedriver', options=chrome_options)
-    return driver
+driver = webdriver.Chrome("./chromedriver", options=chrome_options)
 
 
-def get_content():
-    driver.get("https://webscraper.io/test-sites/e-commerce/allinone")
+def get_content(webpage="https://webscraper.io/test-sites/e-commerce/allinone"):
+    driver.get(webpage)
 
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
@@ -24,24 +21,25 @@ def get_content():
     body = soup.body.text
     head = soup.head.text
 
-    print(head, title, body)
+    #print(head, title, body)
+    #print(head.strip())
     return soup
 
 
 def get_all_tags(content, tag='h1'):
     all_tags = []
 
-    for element in content.select(str(tag)):
-        all_tags.append(element.text)
+    for element in content.select(tag):
+        all_tags.append(element.text.strip())
 
-    print(all_tags)
-    # return all_tags
+    # print(all_tags)
+    return all_tags
 
 
-def get_items(content):
+def get_items(content, product_container='div.thumbnail'):
     top_items = []
-    # Extract and store in top_items according to instructions on the left
-    products = content.select('div.thumbnail')
+
+    products = content.select(product_container)
     for elem in products:
         title = elem.select('h4 > a.title')[0].text
         review_label = elem.select('div.ratings')[0].text
@@ -71,12 +69,12 @@ def get_links(content):
     # return(all_links)
 
 
-def get_all_products(content):
+def get_all_products(content, content_container='div.thumbnail'):
     all_products = []
 
     # Extract and store in top_items according to instructions on the left
 
-    products = content.select('div.thumbnail')
+    products = content.select(content_container)
     for product in products:
         name = product.select('h4 > a')[0].text.strip()
         description = product.select('p.description')[0].text.strip()
@@ -92,6 +90,9 @@ def get_all_products(content):
             "image": image
         })
 
+    # print(all_products)
+    return all_products
+
 
 def save_product_csv(all_products):
     keys = all_products[0].keys()
@@ -103,12 +104,33 @@ def save_product_csv(all_products):
 
 
 if __name__ == "__main__":
+    # TODO: learn how to use 'class' vs 'def' in python
     # TODO: make program able to access different ecommerce sites: https://webscraper.io/test-sites
-    print("WebScraper.py")
+    print("WebScraper.py\n")
 
-    driver = get_driver()
-    driver.get('https://webscraper.io/test-sites/e-commerce/allinone')
-    time.sleep(2)
-    driver.get('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops')
-    time.sleep(2)
+    url_home = 'https://webscraper.io/test-sites/e-commerce/allinone'
+    url_computers_top_items = 'https://webscraper.io/test-sites/e-commerce/allinone/computers'
+    url_laptops = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
+    url_tablets = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+    url_phones = "https://webscraper.io/test-sites/e-commerce/allinone/phones"
+    url_phones_touch = "https://webscraper.io/test-sites/e-commerce/allinone/phones/touch"
+
+    urls = [url_home,
+            url_computers_top_items,
+            url_laptops,
+            url_tablets,
+            url_phones,
+            url_phones_touch]
+
+    for url in urls:
+        html = get_content(url)
+        element_tags = get_all_tags(html, "h1.page-header")
+        # print(tags)
+        for element_tag in element_tags:
+            print(element_tag)
+
+        products = get_all_products(html)
+        for product in products:
+            print(product)
+
     driver.quit()
